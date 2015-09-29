0a91d8cdfe51e44c24f6ae7326df267037d09167
Rui Gonçalves <ruippeixotog@gmail.com>
2015-09-29 01:14:18 +0100

    Add some error handling in Express

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ var app = expressWs(express());
 
 app.use('/tictactoe', gameRoute(TicTacToe));
 
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('An internal error occurred');
+});
+
 var server = app.listen(3000, function () {
   console.log('Example app listening on port %s', server.address().port);
 });
