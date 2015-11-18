ce3f74070e1644a369862e4c39a2198f8e5bb58e
Rui Gonçalves <ruippeixotog@gmail.com>
2015-11-18 22:04:44 +0000

    Update deprecated sendfile call

--- server/main.js
+++ server/main.js
@@ -19,7 +19,7 @@ _.each(config.games, (gameInfo, gameId) => {
 app.use(express.static("dist"));
 
 app.get(/^(?!\/api)/, function (req, res) {
-  res.sendfile("dist/index.html");
+  res.sendFile("dist/index.html", { root: __dirname + "/.." });
 });
 
 let server = app.listen(3000, function () {
