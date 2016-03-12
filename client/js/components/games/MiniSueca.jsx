a3b16db18bffec5e036c22c8183a1a6476c98f1f
Pedro Fernandes <pedro.fernandes@blip.pt>
2016-03-12 17:01:48 +0000

    add miniature version of sueca with only 3 cards

---
+++ client/js/components/games/MiniSueca.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+
+import _ from "underscore";
+
+const playerInfo = [
+  { x: 0, y: 1, labelTop: 90, labelLeft: 32 },
+  { x: 1, y: 0, labelTop: 70, labelLeft: 82 },
+  { x: 0, y: -1, labelTop: 7.5, labelLeft: 68 },
+  { x: -1, y: 0, labelTop: 30, labelLeft: 18 }
+];
+
+class Card extends React.Component {
+  getRankClass(value) {
+    switch (value) {
+      case "A": return "rank1";
+      case "K": return "rank13";
+      case "Q": return "rank12";
+      case "J": return "rank11";
+      default: return `rank${value}`;
+    }
+  }
+
+  render() {
+    let { card, top, left, zIndex, onClick, ...props } = this.props;
+    let cardClasses = "card " + (card ? ` ${card.suit} ${this.getRankClass(card.value)}` : "");
+    return (
+        <div className={cardClasses} style={{ top: `${top}%`, left: `${left}%`, zIndex }}
+             onClick={onClick ? () => onClick(card) : null} {...props}>
+          <div className={ card ? "face" : "back" }></div>
+        </div>
+    );
+  }
+}
+
+const CurrentTrick = ({ cards, lastTrickCards, delta = 10, rot }) => {
+  if (!cards) return <div className="curr-trick" />;
+
+  if (_(cards).every(c => c == null) && lastTrickCards)
+    cards = lastTrickCards;
+
+  let cardElems = [];
+  for (let i = 0; i < 4; i++) {
+    if (!cards[i]) continue;
+    let pi = (i + rot) % 4;
+    cardElems.push(<Card card={cards[i]} key={`trick${i}`} zIndex={10 + i}
+                         top={50 + playerInfo[pi].y * delta}
+                         left={50 + playerInfo[pi].x * delta} />);
+  }
+
+  return (
+      <div className="curr-trick">
+        {cardElems}
+      </div>
+  );
+};
+
+const PlayerLabel = ({ player, nextPlayer, top, left }) => {
+  let team = player % 2 ? 1 : 2;
+  let isCurrentPlayer = player === nextPlayer;
+  return (
+      <div className={`player-chip team${team} ${isCurrentPlayer ? "current-player" : ""}`}
+           style={{ top: `${top}%`, left: `${left}%` }}>{player}</div>
+  );
+};
+
+const Hand = ({ player, nextPlayer, cards, cardCount, deltaX = 40, deltaY = 35,
+    deltaCx = 2, deltaCy = 3, onCardClick, rot }) => {
+
+  let info = playerInfo[((player - 1) + rot) % 4];
+  let x = 50 + info.x * deltaX - info.y * deltaCx * 4.5;
+  let y = 50 + info.y * deltaY - info.x * deltaCy * 4.5;
+
+  function cardIndex(card) {
+    switch (card.value) {
+      case "A": return 10;
+      case "7": return 9;
+      case "K": return 8;
+      case "J": return 7;
+      case "Q": return 6;
+      default: return parseInt(card.value) - 1;
+    }
+  }
+
+  function cardCompare(card1, card2) {
+    if (card1.suit !== card2.suit) return card1.suit < card2.suit ? -1 : 1;
+    return cardIndex(card1) - cardIndex(card2);
+  }
+
+  let sortedCards = cards ? cards.sort(cardCompare) : {};
+
+  let cardElems = [];
+  for (let i = 0; i < cardCount; i++) {
+    cardElems.push(<Card card={sortedCards[i]} top={y} left={x} zIndex={i} key={`card${i}`}
+                         onClick={onCardClick} />);
+    x += info.y * deltaCx;
+    y += info.x * deltaCy;
+  }
+
+  return (
+      <div className={`hand ${onCardClick ? "playable-hand" : ""}`}>
+        <PlayerLabel player={player} nextPlayer={nextPlayer}
+                     top={info.labelTop} left={info.labelLeft} />
+        {cardElems}
+      </div>
+  );
+};
+
+const cardNumber = 3;
+
+const Hands = ({ player, nextPlayer, handCards, tricksDone, currentTrick, rot, onCardClick }) => {
+  if (!currentTrick) return <div className="hands" />;
+
+  let hands = [];
+  for (let i = 0; i < 4; i++) {
+    if (i === player - 1) {
+      hands.push(<Hand player={i + 1} nextPlayer={nextPlayer} cards={handCards}
+                       cardCount={handCards.length} key={`hand${i}`} rot={rot}
+                       onCardClick={onCardClick ? card => onCardClick(card, i) : null} />)
+    } else {
+      let cardCount = cardNumber - tricksDone - (currentTrick[i] ? 1 : 0);
+      hands.push(<Hand player={i + 1} nextPlayer={nextPlayer} cardCount={cardCount}
+                       key={`hand${i}`} rot={rot} />);
+    }
+  }
+
+  return (
+      <div className="hands">
+        {hands}
+      </div>
+  );
+};
+
+const Trump = ({ card, player }) => {
+  if (!card) return <div className="trump" />;
+
+  return (
+      <div className="trump">
+        <span>Trump (player {player || "-"})</span><br />
+        <Card card={card} top={250} left={50} zIndex={10} />
+      </div>
+  );
+};
+
+const LastTrick = ({ cards, x = 10, y = 85, delta = 5, rot }) => {
+  if (!cards) cards = [];
+
+  let cardElems = cards.map((c, i) => {
+    let pi = (i + rot) % 4;
+    return <Card card={c} key={`trick${i}`} zIndex={10 + i}
+                 top={y + playerInfo[pi].y * delta} left={x + playerInfo[pi].x * delta} />;
+  });
+
+  return (
+      <div className="last-trick">
+        {cardElems}
+      </div>
+  );
+};
+
+const Scoreboard = ({ score }) => (
+    <div className="scoreboard">
+      <span>Score</span>
+      <br />
+      <span><span className="player-chip inline team1" />Team 1: {score ? score[0] : "-"}</span>
+      <br />
+      <span><span className="player-chip inline team2" />Team 2: {score ? score[1] : "-"}</span>
+    </div>
+);
+
+const MiniSueca = ({ player, gameState, isLastState, onMove }) => {
+  let { nextPlayer, hand, currentTrick, lastTrick, tricksDone,
+      trump, trumpPlayer, score } = gameState || {};
+
+  let rot = player ? (5 - player) % 4 : 0;
+  let onCardClick = isLastState && player === nextPlayer ? onMove : null;
+
+  return (
+      <Row className="flex">
+        <Col lg={12} className="flex">
+          <div id="mini-sueca" className="flex">
+            <div className="deck flex">
+              <CurrentTrick cards={currentTrick} lastTrickCards={lastTrick} rot={rot} />
+              <Hands player={player} nextPlayer={nextPlayer} handCards={hand}
+                     tricksDone={tricksDone} currentTrick={currentTrick} rot={rot}
+                     onCardClick={onCardClick} />
+              <Trump card={trump} player={trumpPlayer} />
+              <LastTrick cards={lastTrick} rot={rot} />
+              <Scoreboard score={score} />
+            </div>
+          </div>
+        </Col>
+      </Row>);
+};
+
+export default MiniSueca;
---
+++ server/models/games/mini-sueca.js
@@ -0,0 +1,156 @@
+import _ from "underscore";
+
+import Game from "./game";
+
+const cardData = {
+  "2": { index: 0, points: 0 },
+  // "3": { index: 1, points: 0 },
+  // "4": { index: 2, points: 0 },
+  // "5": { index: 3, points: 0 },
+  // "6": { index: 4, points: 0 },
+  "Q": { index: 5, points: 2 },
+  // "J": { index: 6, points: 3 },
+  "K": { index: 7, points: 4 },
+  // "7": { index: 8, points: cardNumber },
+  // "A": { index: 9, points: 11 }
+};
+
+const cardNumber = Object.keys(cardData).length;
+
+class MiniSueca extends Game {
+  constructor(params = {}) {
+    super(params);
+
+    let deck = MiniSueca.generateDeck();
+
+    this.hands = new Array(4);
+    for (let i = 0; i < 4; i++) {
+      this.hands[i] = deck.slice(cardNumber * i, cardNumber * (i + 1));
+    }
+    this.trumpPlayer = Math.floor(Math.random() * 4 + 1);
+    this.trump = this.hands[this.trumpPlayer - 1][0];
+
+    this.nextPlayer = MiniSueca.getPlayerAfter(this.trumpPlayer);
+    this.score = [0, 0];
+    this.winners = null;
+    this.error = false;
+
+    this.trickSuit = null;
+    this.lastTrick = null;
+    this.currentTrick = [null, null, null, null];
+    this.tricksDone = 0;
+  }
+
+  getPlayerCount() { return 4; }
+  isEnded() { return this.error || this.winners !== null || this.tricksDone === cardNumber; }
+  isError() { return this.error; }
+  getWinners() { return this.winners; }
+  getNextPlayer() { return this.nextPlayer; }
+
+  isValidMove(player, card) {
+    return player === this.nextPlayer && this._canPlay(player, card);
+  }
+
+  move(player, card) {
+    if (!this.isValidMove(player, card)) {
+      this.error = true;
+    } else {
+      this.trickSuit = this.trickSuit || card.suit;
+      this.currentTrick[player - 1] = card;
+      this.hands[player - 1] = _(this.hands[player - 1]).reject(MiniSueca.cardEquals(card));
+
+      this.nextPlayer = MiniSueca.getPlayerAfter(player);
+      if (this.currentTrick[this.nextPlayer - 1])
+        this._endTrick();
+    }
+  }
+
+  getFullState() {
+    return {
+      nextPlayer: this.isEnded() ? null : this.nextPlayer,
+      hands: this.hands,
+      trumpPlayer: this.trumpPlayer,
+      trump: this.trump,
+      lastTrick: this.lastTrick,
+      currentTrick: this.currentTrick,
+      trickSuit: this.trickSuit,
+      tricksDone: this.tricksDone,
+      score: this.score,
+      winners: this.winners,
+      isError: this.error
+    };
+  }
+
+  getStateView(fullState, player) {
+    return {
+      ...fullState,
+      hands: undefined,
+      hand: fullState.hands[player - 1]
+    };
+  }
+
+  onMoveTimeout() {
+    this.winners = MiniSueca.getTeam(this.nextPlayer) === 1 ? [1, 3] : [2, 4];
+    this.nextPlayer = null;
+    return true;
+  }
+
+  _canPlay(player, card) {
+    if (!_(this.hands[player - 1]).some(MiniSueca.cardEquals(card))) return false;
+    if (!this.trickSuit) return true;
+    return card.suit === this.trickSuit
+        || _(this.hands[player - 1]).every(c => c.suit !== this.trickSuit);
+  }
+
+  _endTrick() {
+    const trickCardsWhere = props =>
+        _.chain(this.currentTrick).where(props).sortBy(c => -cardData[c.value].index).value();
+
+    let winnerCard;
+    let trumpCards = trickCardsWhere({ suit: this.trump.suit });
+    if (trumpCards.length > 0) {
+      winnerCard = trumpCards[0];
+    } else {
+      let trickSuitCards = trickCardsWhere({ suit: this.trickSuit });
+      winnerCard = trickSuitCards[0];
+    }
+
+    let winnerPlayer = this.currentTrick.findIndex(MiniSueca.cardEquals(winnerCard)) + 1;
+    this.score[MiniSueca.getTeam(winnerPlayer) - 1] +=
+        _(this.currentTrick).reduce((acc, c) => acc + cardData[c.value].points, 0);
+
+    this.trickSuit = null;
+    this.lastTrick = this.currentTrick;
+    this.currentTrick = [null, null, null, null];
+
+    if (++this.tricksDone === cardNumber) {
+      this.nextPlayer = null;
+      if (this.score[0] !== this.score[1]) {
+        this.winners = this.score[0] > this.score[1] ? [1, 3] : [2, 4];
+      } else {
+        this.winners = [];
+      }
+    } else {
+      this.nextPlayer = winnerPlayer;
+    }
+  }
+
+  static generateDeck() {
+    return _.chain(["clubs", "diamonds", "hearts", "spades"]).map(suit =>
+        _(cardData).keys().map(value => ({ suit, value }))
+    ).flatten().shuffle().value();
+  }
+
+  static getPlayerAfter(player) {
+    return player % 4 + 1;
+  }
+
+  static getTeam(player) { return (player - 1) % 2 + 1; }
+
+  static cardEquals(card) {
+    if (!card) return () => false;
+    return c => c && c.suit === card.suit && c.value === card.value;
+  }
+}
+
+export default MiniSueca;
