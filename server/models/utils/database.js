39d6fea3313c332df7ba3d384ef4186df9ee0e0b
Pedro Fernandes <pedro.fernandes@blip.pt>
2016-03-26 20:23:37 +0000

    refactor database to handle non existing bucket or other init errors

--- server/models/utils/database.js
+++ server/models/utils/database.js
@@ -2,59 +2,112 @@ import fs from "fs";
 import { Cluster, N1qlQuery } from "couchbase"
 import Promise from "bluebird";
 
-const config = JSON.parse(fs.readFileSync("config.json"));
+const dbConfig = JSON.parse(fs.readFileSync("config.json")).couchbase;
 
 function getNoopFunction(value) {
   return () => value;
 }
 
-let database = {
-  games: {
-    save: getNoopFunction(),
-    getAll: getNoopFunction(Promise.resolve([]))
-  },
-  competitions: {
-    save: getNoopFunction(),
-    getAll: getNoopFunction(Promise.resolve([]))
+function getNoopDatabase() {
+  return {
+    games: {
+      save: getNoopFunction(),
+      getAll: getNoopFunction(Promise.resolve([]))
+    },
+    competitions: {
+      save: getNoopFunction(),
+      getAll: getNoopFunction(Promise.resolve([]))
+    }
   }
-};
+}
 
-if (config.couchbase.enabled) {
-
-  let cluster = new Cluster(config.couchbase.cluster);
-  let bucket = cluster.openBucket(config.couchbase.bucket);
+function getWorkingDatabase(bucket) {
 
   let bucketQuery = Promise.promisify(bucket.query, { context: bucket });
   let bucketUpsert = Promise.promisify(bucket.upsert, { context: bucket });
 
-  database.games.save = function (object) {
+  function saveGame(object) {
     object.type = "game";
     return bucketUpsert(object.id, object);
-  };
+  }
 
-  database.games.getAll =  function (gameClassName) {
+  function getAllGames(gameClassName) {
     let query = N1qlQuery.fromString(`
-        SELECT default.* FROM default
-        where type = "game" and game.gameClass = "${gameClassName}"
-      `);
-    return bucketQuery(query);
-  };
-
-  database.competitions.save = function (object) {
-    object.type = "competition";
-    return bucketUpsert(object.id, object);
-  };
-
-  database.competitions.getAll = function (gameClassName) {
-    let query = N1qlQuery.fromString(`
-        select default.*
-        from default
-        where type = "competition"
-        and ANY gameInstance IN OBJECT_VALUES(gameRegistry.instances)
-        SATISFIES gameInstance.game.gameClass = "${gameClassName}" END
-      `);
+      SELECT default.* FROM default
+      where type = "game" and game.gameClass = "${gameClassName}"
+    `);
     return bucketQuery(query);
   }
+
+  function saveCompetition(object) {
+    object.type = "competition";
+    return bucketUpsert(object.id, object);
+  }
+
+  function getAllCompetitions(gameClassName) {
+    let query = N1qlQuery.fromString(`
+      select default.*
+      from default
+      where type = "competition"
+      and ANY gameInstance IN OBJECT_VALUES(gameRegistry.instances)
+      SATISFIES gameInstance.game.gameClass = "${gameClassName}" END
+    `);
+    return bucketQuery(query);
+  }
+
+  return {
+    games: {
+      save: saveGame,
+      getAll: getAllGames
+    },
+    competitions: {
+      save: saveCompetition,
+      getAll: getAllCompetitions
+    }
+  }
 }
+
+let databaseInstancePromise = new Promise(function (resolve /*, reject*/) {
+
+  if (!dbConfig.enabled) {
+    resolve(getNoopDatabase());
+    return;
+  }
+
+  let cluster = new Cluster(dbConfig.cluster);
+
+  let bucket = cluster.openBucket(dbConfig.bucket, dbConfig.password, function (error) {
+
+    if (error) {
+      console.error(`Please create the bucket in your couchbase server first
+        Don't forget to create index as well!
+        Cancelling database use for this run.`);
+      resolve(getNoopDatabase());
+      return;
+    }
+
+    resolve(getWorkingDatabase(bucket));
+  });
+});
+
+let database = {
+  games: {
+    save: function (object) {
+      return databaseInstancePromise.then(dbInstance => dbInstance.games.save(object));
+    },
+    getAll: function (gameClassName) {
+      return databaseInstancePromise.then(dbInstance => dbInstance.games.getAll(gameClassName));
+    }
+  },
+  competitions: {
+    save: function (object) {
+      return databaseInstancePromise.then(dbInstance => dbInstance.competitions.save(object));
+    },
+    getAll: function (gameClassName) {
+      return databaseInstancePromise
+        .then(dbInstance => dbInstance.competitions.getAll(gameClassName));
+    }
+  }
+};
 
 export default database;
